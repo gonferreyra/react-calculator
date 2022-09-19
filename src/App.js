89a0b62e7110b9413448a54cc62127b52f13263e
Gonzalo Ferreyra <gonferreyra@gmail.com>
2022-09-19 10:23:20 -0300

    uploading files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+
+const App = () => {
+
+    const [result, setResult] = useState()
+
+    return (
+        <>
+            <h1>Hello World</h1>
+        </>
+    )
+}
+export default App
